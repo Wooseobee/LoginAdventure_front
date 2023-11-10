498c92f177d27e05d03a4d9a5221fc6e82278000
jongwoop97@gmail.com <jongwoop97@gmail.com>
2023-11-10 17:50:52 +0900

    plan register modal 진행중

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -33,21 +33,6 @@
       <div id="map" style="width: 100%; height: 100%"></div>
     </main>
 
-    <!-- 여행계획 등록 Modal -->
-    <div id="planRegistModal" class="modalWrap">
-      <div id="modalBody" class="modalBody">
-        <span id="closePlanRegistBtn" class="closeBtn">&times;</span>
-        <div class="form">
-          <h1 style="margin-bottom: 20px; font-size: 18px">여행계획 등록</h1>
-          <form name="planRegist-form" class="planRegist-form">
-            <input id="planTitle" type="text" placeholder="제목" />
-            <textarea id="planContent" placeholder="내용" rows="5"></textarea>
-            <button id="planRegist-submit" type="button">등록</button>
-          </form>
-        </div>
-      </div>
-    </div>
-
     <!-- 여행계획 view Modal -->
     <div id="planViewModal" class="modalWrap">
       <div id="modalBody" class="modalBody">
---
+++ src/views/planRegistModal.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+  <!-- 여행계획 등록 Modal -->
+  <div id="planRegistModal" class="modalWrap">
+    <div id="modalBody" class="modalBody">
+      <span id="closePlanRegistBtn" class="closeBtn">&times;</span>
+      <div class="form">
+        <h1 style="margin-bottom: 20px; font-size: 18px">여행계획 등록</h1>
+        <form name="planRegist-form" class="planRegist-form">
+          <input id="planTitle" type="text" placeholder="제목" />
+          <textarea id="planContent" placeholder="내용" rows="5"></textarea>
+          <button id="planRegist-submit" type="button">등록</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
