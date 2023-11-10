3da8173050bae0cbd26bc628e1fdc68fbd6346ef
jongwoop97@gmail.com <jongwoop97@gmail.com>
2023-11-10 13:51:56 +0900

    trip plan tab start

---
+++ src/views/TripPlanTab.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <nav>
+    <button id="searchPlaceBtn">관광지 탐색</button>
+    <button id="myPlanBtn">나의 여행계획</button>
+  </nav>
+</template>
+
+<style scoped></style>
