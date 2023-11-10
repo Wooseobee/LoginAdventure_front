14f983caac25337b16cf791848dbb627a04c0fbf
jongwoop97@gmail.com <jongwoop97@gmail.com>
2023-11-10 13:31:10 +0900

    fix: remove kakaomap import

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <script setup>
   import Sidebar from '@/components/common/Sidebar.vue';
-  import kakaoMap from '@/components/kakaoMap.vue';
   import KMap from '@/components/KMap.vue';
 </script>
 
